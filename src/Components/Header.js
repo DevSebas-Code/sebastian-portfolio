189e1fa2f6b7904d38a45bf4c23f2e43ea7fcd0c
Sebatian Diaz <sebastian.diaz.ortizdezarate@gmail.com>
2023-07-30 23:36:07 +0100

    some change in order to improve the video rendering

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -21,7 +21,7 @@ class Header extends Component {
 
     return (
       <header id="home">
-        <video className="video-background" autoPlay loop muted preload="auto">
+        <video className="video-background" autoPlay loop muted>
           <source src={backgroundVideoUrl} type="video/mp4" />
           {/* Add additional <source> elements for other supported video formats */}
         </video>
